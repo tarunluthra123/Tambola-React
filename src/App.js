8df6a7bece0280c5176668281d78d04791e64578
Tarun Luthra <tarunluthra987@gmail.com>
2020-07-15 16:53:09 +0530

    First commit - Added random number generator with animation effect

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <button class="btn btn-success">Click</button>
+            </div>
+        );
+    }
+}
+
+export default App;
